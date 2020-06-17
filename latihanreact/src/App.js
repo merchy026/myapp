b5bd7bfa7c50f6a413e0351bc8340afa6d2ae4d2
angela marici <mariciangela07@gmail.com>
2020-06-17 17:05:09 +0700

    Props pada React.Js

--- latihanreact/src/App.js
+++ latihanreact/src/App.js
@@ -1,15 +1,14 @@
  import React from 'react';
 import logo from './logo.svg';
 import Home from './Comonent/Fungsional/Home';
-import Beranda from './Comonent/Class/Beranda';
+//import Beranda from './Comonent/Class/Beranda';
 //import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Home/>
-     <Beranda/>
-      </div>
+   </div>
   );
 }
 
--- latihanreact/src/Comonent/Fungsional/Home.jsx
+++ latihanreact/src/Comonent/Fungsional/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Blog from './Blog'
 
 const Home = () => {
-    return <div><h1>Ini adalah Home</h1></div>
+    return <div>
+        <Blog
+            tanggal="17 Juni 2020"
+            judul="Teknologi Blockchain"
+            summary="Generate Lorem Ipsum placeholder text for use in your graphic."
+        />
+        <Blog
+            tanggal="18 Juni 2020"
+            judul="Teknologi Internet of Things"
+            summary="Generate Lorem Ipsum placeholder text for use in your graphic."
+        />
+        <Blog
+            tanggal="19 Juni 2020"
+            judul="Desain Patter Laravel"
+            summary="Generate Lorem Ipsum placeholder text for use in your graphic."
+        />
+
+    </div>
 }
 
 export default Home;
