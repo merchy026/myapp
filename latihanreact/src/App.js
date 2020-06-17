ce062c301c034b1e424ea2827e444cfb37bc8930
angela marici <mariciangela07@gmail.com>
2020-06-17 12:58:22 +0700

    Komponen React.js

--- latihanreact/src/App.js
+++ latihanreact/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
+ import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Home from './Comonent/Fungsional/Home';
+import Beranda from './Comonent/Class/Beranda';
+//import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Home/>
+     <Beranda/>
+      </div>
   );
 }
 
 export default App;
+ 
