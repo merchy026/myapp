1d36a83d3979d8c90482fba7cb0cf708e39b0b1d
angela marici <mariciangela07@gmail.com>
2020-07-22 16:58:58 +0700

    React Hooks useReducer + Context

--- latihanreact/src/App.js
+++ latihanreact/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext, useReducer } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 //import BoostrapComp from './Comonent/Class/BootstrapComp';
 import NavbarComp from './Comonent/Fungsional/NavbarComp';
@@ -15,19 +15,48 @@ import HooksUseEffects from './Comonent/Hooks/Functional/HooksUseEffects';
 import { CartContext } from './CardContext';
 import ProductComp from './Comonent/Hooks/Functional/ProductComp';
 import HooksRecucer from './Comonent/Hooks/Functional/HookReducer';
+import Tagihan from './Comonent/Hooks/Functional/Tagihan';
 
 //import Home from './Comonent/Fungsional/Home';
 //import Beranda from './Comonent/Class/Beranda';
 //import './App.css';
 
+export const keranjangContext = createContext()
+
+const initialState = {
+  jumlah: 1,
+  hargasatuan: 10000,
+  hargatotal: 10000
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+      case 'tambah': return {
+          ...state,
+          jumlah: state.jumlah + 1,
+          hargatotal: state.hargasatuan + (state.hargasatuan * state.jumlah)
+      }
+      case 'kurang': return {
+          ...state,
+          jumlah: state.jumlah - 1,
+          hargatotal: (state.hargasatuan * state.jumlah) - state.hargasatuan
+      }
+      default:
+          return state
+  }
+}
+
 const App = () => {
 
   const[value, setValue] = useState(0)
+
+  const [count, dispatch] = useReducer(reducer, initialState)
 
   return (
     <BrowserRouter>
       <CartContext.Provider value={{value, setValue}}> 
         <NavbarComp />
+        <keranjangContext.Provider value={{keranjangState: count, keranjangDispatch:dispatch}}>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/about" component={About} />
@@ -39,10 +68,11 @@ const App = () => {
           <Route exact path="/useeffects" component={HooksUseEffects} />
           <Route exact path="/produk" component={ProductComp} />
           <Route exact path="/reducer" component= {HooksRecucer} />
+          <Route exact path="/tagihan" component= {Tagihan} />
 
           {/* <Route exact path="/detail/:id" component={DetailComp} /> */}
-
         </Switch>
+        </keranjangContext.Provider>
       </CartContext.Provider>
     </BrowserRouter>
 
--- latihanreact/src/Comonent/Hooks/Functional/HookReducer.jsx
+++ latihanreact/src/Comonent/Hooks/Functional/HookReducer.jsx
@@ -1,31 +1,12 @@
 import React, { useReducer } from 'react'
 import { Container, Row, Col, Card, CardImg, Button } from 'reactstrap'
+import {NavLink} from 'react-router-dom'
+import { useContext } from 'react'
+import { keranjangContext } from '../../../App'
 
-const initialState = {
-    jumlah: 1,
-    hargasatuan: 10000,
-    hargatotal: 10000
-}
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'tambah': return {
-            ...state,
-            jumlah: state.jumlah + 1,
-            hargatotal: state.hargasatuan + (state.hargasatuan * state.jumlah)
-        }
-        case 'kurang': return {
-            ...state,
-            jumlah: state.jumlah - 1,
-            hargatotal: (state.hargasatuan * state.jumlah) - state.hargasatuan
-        }
-        default:
-            return state
-    }
-}
 function HooksRecucer() {
 
-    const [count, dispatch] = useReducer(reducer, initialState)
+    const countContext = useContext(keranjangContext)
     return (
         <Container>
             <br />
@@ -38,15 +19,17 @@ function HooksRecucer() {
                 <Col xs="6">
                     <h3>Action Figur Naruto</h3>
                     <p>Harga</p>
-                    <h3>Rp. {count.hargasatuan}</h3>
+                    <h3>Rp. {countContext.keranjangState.hargatotal}</h3>
                     <p>Jumlah</p>
                     <Row>
-                        <Col><Button onClick={() => dispatch({ type: 'tambah' })} color="danger">+</Button></Col>
-                        <Col>{count.jumlah}</Col>
-                        <Col><Button onClick={() => dispatch({ type: 'kurang' })}>-</Button></Col>
+                        <Col><Button onClick={() => countContext.keranjangDispatch({ type: 'tambah' })} color="danger">+</Button></Col>
+                        <Col>{countContext.keranjangState.jumlah}</Col>
+                        <Col><Button onClick={() => countContext.keranjangDispatch({ type: 'kurang' })}>-</Button></Col>
                     </Row>
                     <br />
-                    <Button color="success" size="lg">{count.hargatotal}</Button>
+                    <Button color="success" size="lg"> Total Rp. {countContext.keranjangState.hargatotal}</Button>
+                    <hr/>
+                    <NavLink to='/tagihan'>Tagihan Belanja</NavLink>
                 </Col>
             </Row>
         </Container>
